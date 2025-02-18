7538d2794db6b7d07cc36dcc70d0d6801f72188c
Aleksey Kulushev <alexcoolarbitr@gmail.com>
2025-02-18 23:18:31 +0300

    Правки по замечаниям.

--- src/Main.java
+++ src/Main.java
@@ -3,62 +3,42 @@ public class Main {
         System.out.println("Создадим VersionableStringBuilder");
         VersionableStringBuilder vsb = new VersionableStringBuilder();
 
-        System.out.println("\nПопробуем получить последнее сохраненное состояние");
-        try {
+        System.out.println("\nПопробуем откатить состояние VersionableStringBuilder на шаг назад");
             vsb.undo();
-        } catch (IllegalStateException e) {
-            System.err.println("Произошла ошибка: " + e.getMessage());
-        }
-        System.out.println(vsb.toString());
+        System.out.println("Вывод: " + vsb.toString());
 
+        System.out.println("\nДобавим в VersionableStringBuilder строку");
         vsb.append("Hello World");
-        System.out.println(vsb.toString());
-
-        System.out.println("\nСохраним текущее состояние билдера");
-        vsb.saveState();
+        System.out.println("Вывод: " + vsb.toString());
 
         System.out.println("\nДобавим к текущей записи еще строку");
         vsb.append(", I love you!");
-        System.out.println(vsb.toString());
+        System.out.println("Вывод: " + vsb.toString());
 
-        System.out.println("\nПопробуем восстановить первоначальную запись");
-        try {
+        System.out.println("\nПопробуем откатить состояние VersionableStringBuilder на шаг назад");
             vsb.undo();
-        } catch (IllegalArgumentException e) {
-            System.err.println("Произошла ошибка: " + e.getMessage());
-        }
-        System.out.println(vsb.toString());
+        System.out.println("Вывод: " + vsb.toString());
 
-        System.out.println("\nДобавим к текущей записи еще строку");
+        System.out.println("\nСнова добавим к текущей записи еще пару строк");
         vsb.append(", I love you!");
-        System.out.println(vsb.toString());
-
-        System.out.println("\nСохраним текущее состояние билдера");
-        vsb.saveState();
-
-        System.out.println("\nДобавим к текущей записи еще строку");
+        System.out.println("Вывод: " + vsb.toString());
         vsb.append(" I'am developer!");
-        System.out.println(vsb.toString());
+        System.out.println("Вывод: " + vsb.toString());
 
-        System.out.println("\nПопробуем вернуться на два шага назад");
-        try {
-            vsb.undoSomeStep(2);
-        } catch (IllegalArgumentException e) {
-            System.err.println("Произошла ошибка: " + e.getMessage());
-        }
-        System.out.println(vsb.toString());
+        System.out.println("\nУдалим часть строки");
+        vsb.delete(2,8);
+        System.out.println("Вывод: " + vsb.toString());
 
-        System.out.println("\nУдалим из истории одну запись");
-        vsb.deleteLastContent();
+        System.out.println("\nПопробуем откатить состояние VersionableStringBuilder на шаг назад");
+        vsb.undo();
+        System.out.println("Вывод: " + vsb.toString());
 
-        System.out.println("\nПопробуем вернуться ещё раз на два шага назад");
-        try {
-            vsb.undoSomeStep(2);
-        } catch (IllegalArgumentException e) {
-            System.err.println("Произошла ошибка: " + e.getMessage());
-        }
-        System.out.println(vsb.toString());
+        System.out.println("\nПопробуем откатить состояние VersionableStringBuilder на 8 шагов назад");
+        vsb.undoSomeStep(8);
+        System.out.println("Вывод: " + vsb.toString());
 
-
+        System.out.println("\nПопробуем откатить состояние VersionableStringBuilder на 2 шага назад");
+        vsb.undoSomeStep(2);
+        System.out.println("Вывод: " + vsb.toString());
     }
 }
--- src/VersionableStringBuilder.java
+++ src/VersionableStringBuilder.java
@@ -1,9 +1,10 @@
 
-import java.util.TreeMap;
+import java.util.LinkedList;
 
 public class VersionableStringBuilder {
     private StringBuilder builder;
-    private final Memento memento;
+    private LinkedList<String> history;
+    private final int MAX_HISTORY_SIZE = 20;
 
 
     /**
@@ -11,22 +12,22 @@ public class VersionableStringBuilder {
      */
     public VersionableStringBuilder() {
         this.builder = new StringBuilder();
-        this.memento = new Memento();
+        this.history = new LinkedList<>();
     }
 
     public VersionableStringBuilder(int capacity) {
         this.builder = new StringBuilder(capacity);
-        this.memento = new Memento();
+        this.history = new LinkedList<>();
     }
 
     public VersionableStringBuilder(String str) {
         this.builder = new StringBuilder(str);
-        this.memento = new Memento();
+        this.history = new LinkedList<>();
     }
 
     public VersionableStringBuilder(CharSequence seq) {
         this.builder = new StringBuilder(seq);
-        this.memento = new Memento();
+        this.history = new LinkedList<>();
     }
 
 
@@ -34,67 +35,78 @@ public class VersionableStringBuilder {
      * Делегированные методы
      */
     public int compareTo(StringBuilder another) {
-
         return builder.compareTo(another);
     }
 
     public VersionableStringBuilder append(String str) {
         builder.append(str);
+        saveState(builder.toString());
         return this;
     }
 
     public VersionableStringBuilder append(Object obj) {
         builder.append(obj);
+        saveState(builder.toString());
         return this;
     }
 
     public VersionableStringBuilder append(StringBuffer sb) {
         builder.append(sb);
+        saveState(builder.toString());
         return this;
     }
 
     public VersionableStringBuilder append(CharSequence s) {
         builder.append(s);
+        saveState(builder.toString());
         return this;
     }
 
     public VersionableStringBuilder append(char[] str) {
         builder.append(str);
+        saveState(builder.toString());
         return this;
     }
 
     public VersionableStringBuilder appendCodePoint(int codePoint) {
         builder.appendCodePoint(codePoint);
+        saveState(builder.toString());
         return this;
     }
 
     public VersionableStringBuilder delete(int start, int end) {
         builder.delete(start, end);
+        saveState(builder.toString());
         return this;
     }
 
     public VersionableStringBuilder deleteCharAt(int index) {
         builder.deleteCharAt(index);
+        saveState(builder.toString());
         return this;
     }
 
     public VersionableStringBuilder insert(int offset, Object obj) {
         builder.insert(offset, obj);
+        saveState(builder.toString());
         return this;
     }
 
     public VersionableStringBuilder insert(int offset, String str) {
         builder.insert(offset, str);
+        saveState(builder.toString());
         return this;
     }
 
     public VersionableStringBuilder insert(int offset, char[] str) {
         builder.insert(offset, str);
+        saveState(builder.toString());
         return this;
     }
 
     public VersionableStringBuilder insert(int dstOffset, CharSequence s) {
         builder.insert(dstOffset, s);
+        saveState(builder.toString());
         return this;
     }
 
@@ -108,6 +120,7 @@ public class VersionableStringBuilder {
 
     public VersionableStringBuilder reverse() {
         builder.reverse();
+        saveState(builder.toString());
         return this;
     }
 
@@ -118,22 +131,21 @@ public class VersionableStringBuilder {
     /**
      * Сохранить состояние StringBuilder
      */
-    public void saveState() {
-        memento.save(builder.toString());
+    public void saveState(String str) {
+        if (history.size() == MAX_HISTORY_SIZE) {
+            history.removeFirst();
+        }
+        history.addLast(str);
     }
 
     /**
-     * Вернуть StringBuilder в состояние на момент последнего сохранения
-     *
-     * @return StringBuilder в состоянии на момент последнего сохранения
-     * @throws IllegalStateException если сохраненные состояния отсутствуют
+     * @return StringBuilder в состоянии на шаг назад
      */
     public VersionableStringBuilder undo() {
-        var lastContent = memento.getLastContent();
-        if (lastContent == null) {
-            throw new IllegalStateException("Нет сохраненных состояний");
-        }
-        builder = new StringBuilder(lastContent);
+        if (history.isEmpty()) {return this;}
+
+        history.removeLast();
+        builder = new StringBuilder(history.getLast());
         return this;
     }
 
@@ -143,78 +155,20 @@ public class VersionableStringBuilder {
      *
      * @param step количество шагов назад
      * @return StringBuilder в состоянии на n шагов назад
-     * @throws IllegalArgumentException если шаг больше количества сохраненных состояний
      */
     public VersionableStringBuilder undoSomeStep(int step) {
-        var content = memento.getSomeContent(step);
-        if (content == null) {
-            throw new IllegalArgumentException("Нет столько сохраненных состояний");
+        if (history.size() < step) {return this;}
+
+        for (int i = 0; i < step; i++) {
+            history.removeLast();
         }
 
-        builder = new StringBuilder(content);
+        if (history.isEmpty()) {
+            builder = new StringBuilder();
+            return this;
+        }
+
+        builder = new StringBuilder(history.getLast());
         return this;
-    }
-
-    /**
-     * Удалить последний элемент StringBuilder
-     */
-    public void deleteLastContent() {
-        memento.deleteLastContent();
-    }
-
-    private final class Memento {
-        private TreeMap<Integer, String> history;
-
-        private Memento() {
-            history = new TreeMap<Integer, String>();
-        }
-
-        /**
-         * Сохраняет текущее состояние строки
-         *
-         * @param content содержимое для сохранения
-         */
-        private void save(String content) {
-            Integer actualVersion = history.isEmpty() ? 0 : history.lastKey() + 1;
-            history.put(actualVersion, content);
-        }
-
-        /**
-         * Возвращает последнее сохраненное состояние
-         *
-         * @return последнее сохраненное состояние или null, если история пуста
-         */
-        private String getLastContent() {
-            return history.isEmpty() ? null : history.get(history.lastKey());
-        }
-
-        /**
-         * Возвращает состояние на n шагов назад
-         *
-         * @param step количество шагов назад
-         * @return состояние на n шагов назад или null, если шаг больше размера истории
-         */
-        private String getSomeContent(int step) {
-            if (step <= 0) {
-                throw new IllegalArgumentException("Шаг должен быть положительным числом");
-            }
-            var size = history.size();
-            if (size < step) {
-                return null;
-            }
-            return history.get(history.lastKey() - (step - 1));
-        }
-
-        /**
-         * Удалить последний элемент StringBuilder
-         *
-         * @throws IllegalStateException если сохраненные состояния отсутствуют
-         */
-        private void deleteLastContent() {
-            if (history.isEmpty()) {
-                throw new IllegalStateException("Нет сохраненных состояний");
-            }
-            history.remove(history.lastKey());
-        }
     }
 }
