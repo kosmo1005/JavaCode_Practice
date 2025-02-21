b8358717967f948c201fe09ce9c23cf82b32acef
Aleksey Kulushev <alexcoolarbitr@gmail.com>
2025-02-21 16:46:19 +0300

    Filter2

--- src/main/java/com/pr/CustomFilter.java
+++
@@ -1,9 +0,0 @@
-package com.pr;
-
-public class CustomFilter<T> implements Filter<T> {
-    @Override
-    public T apply(T o){
-        System.out.println("Обработка " + o.toString());
-        return o;
-    }
-}
--- src/main/java/com/pr/Main.java
+++ src/main/java/com/pr/Main.java
@@ -1,17 +1,19 @@
 package com.pr;
 
-import java.time.LocalDateTime;
 import java.lang.reflect.Array;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello world!");
 
-        CustomFilter customFilter = new CustomFilter();
         String[] strArr = {null, "b", "c", "d", "e", "f", null};
         Integer[] intArr = {1, 2, 3, 4, null, 6, 7, null, 9, 10};
-        filter(strArr,customFilter);
-        filter(intArr,customFilter);
+
+        String[] filteredStrings = filter(strArr, s -> s + "lol");
+        Integer[] filteredIntegers = filter(intArr, n -> n + 1);
+
+        System.out.println(java.util.Arrays.toString(filteredStrings));
+        System.out.println(java.util.Arrays.toString(filteredIntegers));
 
     }
 
