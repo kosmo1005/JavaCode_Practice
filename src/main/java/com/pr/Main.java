f81bfca207b7a482d75fc5d7bcddd5009bfc1f51
Aleksey Kulushev <alexcoolarbitr@gmail.com>
2025-02-22 19:32:12 +0300

    Main чистый.

--- src/main/java/com/pr/Main.java
+++ src/main/java/com/pr/Main.java
@@ -7,9 +7,7 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello world!");
-        LocalDateTimeViewer localDateTimeViewer = new LocalDateTimeViewer(LocalDateTime.of
-                (2025,2,20,17,8,34, 3_000_000));
-        System.out.println(localDateTimeViewer.format());
+
 
     }
 }
