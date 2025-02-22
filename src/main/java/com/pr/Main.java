2f62c6a84459f709680f10b7def8ab5817dc2d6e
Aleksey Kulushev <alexcoolarbitr@gmail.com>
2025-02-22 19:32:12 +0300

    Main чистый.

--- src/main/java/com/pr/Main.java
+++ src/main/java/com/pr/Main.java
@@ -1,15 +1,9 @@
 package com.pr;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import java.time.LocalDateTime;
-
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello world!");
-        LocalDateTimeViewer localDateTimeViewer = new LocalDateTimeViewer(LocalDateTime.of
-                (2025,2,20,17,8,34, 3_000_000));
-        System.out.println(localDateTimeViewer.format());
+
 
     }
 }
